4bcf8d3283390db9960a91c030ea3366024b875d
Coulsen Anderson <coulsen.anderson@gmail.com>
2022-08-19 12:43:33 -0600

    week1 assessment

---
+++ password.js
@@ -0,0 +1,9 @@
+let password = 'coulsenisthebest2'
+
+
+if(password.length >= 10 && password) 
+    console.log('This password is acceptable.')
+else {
+    console.log('This password is unacceptable.')
+}
+    
---
+++ sdlc.js
@@ -0,0 +1,26 @@
+// In this section you will answer some work through problems related to the Software Development Lifecycle
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
+*/
+
+// Planning
+// Analysis of Requirements
+// Design
+// Implementation
+// Testing & Integration
+// Maintain
+
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
+    The first one is done for you. 
+*/
+const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+const AnalysisOfRequirements = 'Ask questions, understand what every part of the plan is.'
+const Design = 'Creating the architecture, using psuedocode to break each part down.'
+const Implementation = 'Building the software. Write working code down.'
+const TestingAndIntegration = 'Evaluating the quality of software with the aim of finding and fixing bugs.'
+const Maintain = 'Updating and supporting the software after it is released to the public.'
